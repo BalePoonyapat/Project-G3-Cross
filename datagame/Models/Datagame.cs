a7917bd00e8608d2b342d0f980f87eb445c378be
Poonyapat <balepoonyapat@gmail.com>
2022-01-09 03:04:00 +0700

    frist

---
+++ datagame/Models/Datagame.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace datagame.Models
+{
+    public class Datagame
+    {
+        [Key]
+        
+        public string Gamename { get; set; }
+        public string GameType { get; set; }
+        public string Description { get; set; }
+
+    }
+}
---
+++ datagame/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(datagame.Startup))]
+namespace datagame
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
